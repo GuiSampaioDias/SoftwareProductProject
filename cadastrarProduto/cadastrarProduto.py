f3ab140f8d89c6df42158cb5b525cc54d80fe8ab
Joao Alonso <joao.vitoralonso@gmail.com>
2024-03-11 22:08:12 -0300

    Atualizando o comando sql

--- cadastrarProduto/cadastrarProduto.py
+++ cadastrarProduto/cadastrarProduto.py
@@ -1,15 +1,16 @@
 '''
 Lembrando ... quem digita .... erra
 
-create schema restaurante;
+CREATE SCHEMA IF NOT EXISTS restaurante;
 
-use restaurante;
+USE restaurante;
 
-CREATE TABLE tbl_produto 
+CREATE TABLE IF NOT EXISTS tbl_produto 
 ( 
     produto_id     BIGINT       NOT NULL   AUTO_INCREMENT, 
     NomeDoProduto  VARCHAR(45)  NOT NULL,
     Categoria      VARCHAR(15)  NOT NULL, 
+    Quantidade 	   INT          NOT NULL,
     litros         FLOAT(5,2)       NULL, 
     Peso_kg        FLOAT(6,2)       NULL,
     Preço          FLOAT(5,2)   NOT NULL, 
@@ -17,6 +18,9 @@ CREATE TABLE tbl_produto
     Ingredientes   VARCHAR(45), 
     PRIMARY KEY (produto_id)
 );
+
+
+SELECT * FROM tbl_produto
 
 '''
 
