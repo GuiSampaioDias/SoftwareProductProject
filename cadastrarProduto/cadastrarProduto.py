4b6153d385d75c73b1a72c5d7afff50b9587febd
Joao Alonso <joao.vitoralonso@gmail.com>
2024-03-11 21:31:45 -0300

    Arrumando o codigo fonte e implementando o input de quantidade

--- cadastrarProduto/cadastrarProduto.py
+++ cadastrarProduto/cadastrarProduto.py
@@ -30,7 +30,7 @@ app = Flask(__name__)
 
 # MySQL configurations
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'impacta2024'
+app.config['MYSQL_PASSWORD'] = 'Impacta2024'
 app.config['MYSQL_DB'] = 'restaurante'
 app.config['MYSQL_HOST'] = 'localhost'
 #app.config['MYSQL_DATABASE_HOST'] = '172.17.0.7'
@@ -39,7 +39,7 @@ mysql.init_app(app)
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return render_template('formulario_produto.html')
 
 @app.route('/produto')
 def showSignUp():
@@ -51,6 +51,7 @@ def signUp():
     try:
         _nome = request.form['inputNome']
         _categoria = request.form['inputCategoria']
+        _quantidade = request.form['inputQuantidade']
         _litros = request.form['inputLitros']
         _peso = request.form['inputPeso']
         _preco = request.form['inputPreco']
@@ -60,10 +61,11 @@ def signUp():
 
         print(_nome)
         print(_categoria)
+        print(_quantidade)
         print(_litros)
         print(_peso)
         print(_preco)
-        print(_descricao)
+        print(_descricao) 
         print(_ingredientes)
 
         # validate the received values
@@ -72,26 +74,22 @@ def signUp():
             conn = mysql.connection
             cursor = conn.cursor()
             #_hashed_password = _password
-            cursor.execute('insert into tbl_produto (NomeDoProduto, Categoria, litros,Peso_kg, Preço, Descrição, Ingredientes) VALUES (%s, %s, %s, %s, %s, %s, %s)', ( _nome,_categoria,_litros, ))
+            cursor.execute('insert into tbl_produto (NomeDoProduto, Categoria, Quantidade, litros,Peso_kg, Preço, Descrição, Ingredientes) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)', ( _nome,_categoria,_quantidade,_litros,_peso,_preco,_descricao,_ingredientes ))
             conn.commit()
 
-            return render_template('signup.html')
+            return render_template('OK')
         else:
             return json.dumps({'html':'<span>Enter the required fields</span>'})
 
     except Exception as e:
         return json.dumps({'error':str(e)})
-    finally:
-        #cursor.close() 
-        #conn.close()
-        print('ops')
 
 @app.route('/list',methods=['POST','GET'])
 def list():
     try:
             conn = mysql.connect()
             cursor = conn.cursor()
-            cursor.execute ('select NomeDoProduto, Categoria, litros,Peso_kg, Preço, Descrição, Ingredientes from tbl_produto')
+            cursor.execute ('select NomeDoProduto, Categoria, Quantidade, litros,Peso_kg, Preço, Descrição, Ingredientes from tbl_produto')
             data = cursor.fetchall()
             print(data[0])
 
@@ -108,5 +106,4 @@ def list():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-
+    app.run(host='0.0.0.0', port=port, debug=True)
