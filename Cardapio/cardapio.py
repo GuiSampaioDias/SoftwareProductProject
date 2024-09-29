2272f4141d8e2adaeb6391cada105f76e3b9eabe
Joao Alonso <joao.vitoralonso@gmail.com>
2024-09-28 22:43:12 -0300

    Melhorando menu do cadastro de produtos e melhorando o cardapio virtual

--- Cardapio/cardapio.py
+++ Cardapio/cardapio.py
@@ -5,6 +5,9 @@ from flask_mysqldb import MySQL
 mysql = MySQL()
 app = Flask(__name__)
 
+
+upload_folder = os.path.join(os.path.dirname(__file__), '../Cardapio/static/img')
+
 # MySQL configurations
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'Impacta2024'
@@ -19,11 +22,15 @@ def list():
     try:
             conn = mysql.connection
             cursor = conn.cursor()
+            arquivos = os.listdir(upload_folder)
             
             cursor.execute ('SELECT * FROM tblMenu')
             data = cursor.fetchall()
 
-            return render_template('cardapio.html', data = data)
+            arquivos_contados = [f for f in arquivos if os.path.isfile(os.path.join(upload_folder, f))]
+        
+            tamanhoArquivos = len(arquivos_contados)
+            return render_template('cardapio.html', data = data, lenArquivos = tamanhoArquivos)
 
     except Exception as e:
         return json.dumps({'error':str(e)})
