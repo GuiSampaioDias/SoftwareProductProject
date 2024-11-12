cb58045c284aa39c45d7f93842298fc0f32d0cb1
Joao Alonso <joao.vitoralonso@gmail.com>
2024-11-12 17:45:53 -0300

    Teste de config BD fora dos programas principais

--- Cardapio/cardapio.py
+++ Cardapio/cardapio.py
@@ -1,18 +1,16 @@
-import os
+import os, sys
 from flask import Flask, render_template, json, jsonify
 from flask_mysqldb import MySQL
+#import de modulos internos
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import config
 
 mysql = MySQL()
 app = Flask(__name__)
 
-
 upload_folder = os.path.join(os.path.dirname(__file__), '../Cardapio/static/img')
 
-# MySQL configurations
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Impacta2024'
-app.config['MYSQL_DB'] = 'restaurante'
-app.config['MYSQL_HOST'] = 'localhost'
+app.config.from_object(config.Config)
 mysql.init_app(app)
 
 @app.route('/imagens')
---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'Impacta2024'
+    MYSQL_DB = 'restaurante'
+    MYSQL_HOST = 'localhost'
