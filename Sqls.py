9893a8a5f5ec85f8943d1c8786d101680b46005a
Joao Alonso <joao.vitoralonso@gmail.com>
2024-11-18 17:01:59 -0300

    refact: removendo funcoes da pasta estoque

--- Sqls.py
+++ Sqls.py
@@ -23,3 +23,17 @@ def DeleteComWhere(tbl, coluna, valor):
     cursor.execute (f'DELETE FROM {tbl} WHERE {coluna} = %s',(valor, ))
     resposta = conn.commit()
     return resposta
+
+def SelectComOrder(tbl, coluna, ordem, item = '*'):
+    conn, cursor = connCursor()
+    cursor.execute (f'SELECT {item} FROM {tbl} ORDER BY {coluna}  {ordem}')
+    resposta = cursor.fetchall()
+    return resposta
+
+
+def updateParametroMinMax(tbl, min, max, id):
+    conn,cursor = connCursor()
+    cursor.execute(f'UPDATE {tbl} SET min = %s, sugerido = %s WHERE produtoId = %s',(min, max, id))
+    conn.commit()
+
+
